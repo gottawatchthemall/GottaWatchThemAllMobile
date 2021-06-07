fbbd2b4fcc809739f6096fd9fb283cc6bc820285
JeremTer <ternisien09@gmail.com>
2021-06-07 10:37:31 +0200

    first commit

---
+++ GottaWatchThemAllMobile/GottaWatchThemAllMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GottaWatchThemAllMobileApp.swift
+//  GottaWatchThemAllMobile
+//
+//  Created by Jérémy TERNISIEN on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct GottaWatchThemAllMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
