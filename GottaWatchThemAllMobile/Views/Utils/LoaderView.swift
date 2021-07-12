9c6dbacc641cac737e0f792f9e29e5e36d847bc4
Bertho James <james.bertho94@gmail.com>
2021-07-12 21:29:34 +0200

    ADD loading view

---
+++ GottaWatchThemAllMobile/Views/Utils/LoaderView.swift
@@ -0,0 +1,33 @@
+//
+//  LoaderView.swift
+//  GottaWatchThemAllMobile
+//
+//  Created by BERTHO on 09/07/2021.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    
+    var isLoading: Bool
+    
+    var body: some View {
+        Color(.gray).ignoresSafeArea().opacity(0.8)
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            VStack {
+                Text("Chargement en cours").padding(5).foregroundColor(Color.black)
+                ProgressView().progressViewStyle(CircularProgressViewStyle(tint: Color.black))
+                 .scaleEffect(2)
+            }
+        }).frame(width: 220, height: 100)
+        .background(Color("RedPokeball"))
+        .cornerRadius(15.0)
+        
+    }
+}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderView(isLoading: true)
+    }
+}
